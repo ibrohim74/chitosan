9c883526ce10e89a39f33075ea3880c261126bad
ibrohim74 <xibroxim11@gmail.com>
2023-11-08 15:41:29 +0500

    sadas

--- src/component/price.jsx
+++ src/component/price.jsx
@@ -3,12 +3,11 @@ import React from "react";
 const Price =()=>{
     return(
         <div className={'s5 container'}>
-        <div className="s5-left-item ">
+        <div className="s5-left-item " style={{textAlign:"center"}}>
         <div className="s5-title wow animate__animated animate__fadeInUp">
                     <h1>Narxlar</h1>
-                   
                 </div>
-                <h1>1.600.000 so'm</h1>
+                <h1>500.000 so'm</h1>
         </div>
         <div className="s5-right-item wow animate__animated animate__fadeInUp">
         <img src={require('../assets/img/s3/s3-img.png')} alt=""/>
